1312fa9310eec105e943fcabb1b42f553fdb8646
Max Turck <max@grabyo.com>
2020-12-03 18:43:03 +0000

    add helpers

---
+++ utils/main.go
@@ -0,0 +1,35 @@
+package filereader
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+var lines []string
+
+// ReadLines returns a list of strings found in a file
+func ReadLines(filepath string) ([]string, error) {
+	var err error
+	file, err := os.Open(filepath)
+	defer func() {
+		file.Close()
+	}()
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		if err != nil {
+			panic(err)
+		}
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, err
+
+}
